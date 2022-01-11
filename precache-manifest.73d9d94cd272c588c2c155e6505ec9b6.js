09b2e151837f80650dd19b40bf55f53825193529
siddharth <siddharthsharma2@deloitte.com>
2022-01-11 14:04:41 +0530

    first commit

---
+++ precache-manifest.73d9d94cd272c588c2c155e6505ec9b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1a30e210727bc8c72a",
+    "url": "/static/css/main.5478e82c.chunk.css"
+  },
+  {
+    "revision": "4e1a30e210727bc8c72a",
+    "url": "/static/js/main.47fc10f4.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4c4e45c21b9d5021aa11",
+    "url": "/static/js/2.aecb81c7.chunk.js"
+  },
+  {
+    "revision": "fd8c9b80d9212cd961f92ebfa308278e",
+    "url": "/index.html"
+  }
+];
